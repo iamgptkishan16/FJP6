045d1a39519352b768e6822c3b75816b12020e59
iamgptkishan16 <kishan2002001@gmail.com>
2022-09-07 01:54:31 +0530

    functions

---
+++ module2/function.js
@@ -0,0 +1,31 @@
+//function in js without parameter and return type
+
+function sayHello(){
+    console.log("Namaste Bharat");
+}
+sayHello();
+
+
+
+
+//function with return type
+
+function sum(num1, num2){
+    return num1+num2;
+}
+let add = sum(3, 8);
+console.log("Sum = " + add);
+
+
+//storing function in variable
+let store = function(num1, num2){
+    return num1 - num2;
+}
+console.log(store(5, 8)); //functions are first class citizen in js as it can be stored in variables
+
+// function with parameter
+function sum(num1, num2){
+    let addition = num1+num2;
+    console.log("Addition is = " + addition);
+}
+sum(2,3);
