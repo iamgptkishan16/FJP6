d1526156ce4bf0487a809aa5932dbb5eb4485a4d
iamgptkishan16 <kishan2002001@gmail.com>
2022-09-07 00:55:38 +0530

    intro to js

---
+++ module2/intro.js
@@ -0,0 +1,27 @@
+// printing in javascript
+
+console.log("Namaste Bharat");
+// variables in javascript
+
+let a = 10;
+console.log(a);//integer
+
+let char = "abc";
+console.log(char);//character
+
+let str = "I am Kishan Kumar";
+console.log(str);//string
+
+let bool = true;
+console.log(bool);//boolean
+
+for(let i = 1; i <6; i++){
+    console.log(i); 
+}   //for loop
+
+
+let count = 10
+while(count > 0){
+    console.log(count);
+    count--;
+} //while loop
