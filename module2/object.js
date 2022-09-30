230cdcf1ff1dd223aedfe03819b6a3096d9fb538
iamgptkishan16 <kishan2002001@gmail.com>
2022-09-30 13:26:29 +0530

    onject in js

---
+++ module2/object.js
@@ -0,0 +1,28 @@
+//empty object
+let obj = {};
+console.log(obj);
+
+let objKishan = 
+{
+    NAME : "Kishan Kumar",
+    AGE : 21,
+    PHONE_NUMBER : 9110991858,
+    COURSE : "BTECH",
+    "PHONE NUMBER" : 9117393069
+}
+console.log(objKishan);
+
+let kashi = {
+    Name : "Kishan Kumar",
+    Age : 21,
+    Address: {
+        City: "Saharsa",
+        State: "Bihar",
+        Pincode: 852106
+    },
+    Friends: ["ALOK", "SATYAM", "ADARSH"],
+    sayHi : function(){
+        console.log("Hii");
+    }
+}
+console.log(kashi);
