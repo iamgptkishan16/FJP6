4661b9b6cca678bfffa89661c8abfe4fb364dd2f
iamgptkishan16 <kishan2002001@gmail.com>
2022-10-01 01:24:15 +0530

    operations on keys

--- module2/object.js
+++ module2/object.js
@@ -21,6 +21,7 @@ let kashi = {
         Pincode: 852106
     },
     Friends: ["ALOK", "SATYAM", "ADARSH"],
+    isEngineer: false,
     sayHi : function(){
         console.log("Hii");
     }
@@ -33,6 +34,29 @@ console.log(kashi.Name);
 console.log(kashi.sayHi);
 console.log(kashi.Address.city);
 console.log(kashi.Address.City);
-console.log(kashi.sayHi());// it will print undefined as the function is not returning anything. In function above, we have consoe.log not anything returning.
+console.log(kashi.sayHi());// it will print undefined also along with Hii, as the function is not returning anything. In function above, we have consoe.log not anything returning.
 // for function, we can directly call it here
 kashi.sayHi();
+console.logh
+// adding a new key in obj
+kashi.education= ["D.A.V", "J&M Academy", "KEC" ];
+console.log(kashi);
+console.log(kashi.education);
+
+//deleting a key from the object
+console.log("Object before deletion of the key :", kashi);
+
+
+delete kashi.Friends;
+console.log("Object after deletion of key :", kashi);
+
+//updatation of keys
+
+console.log("Object before updatation :", kashi);
+kashi.isEngineer =  true;
+console.log("Object after updatation :", kashi);
+// second method to access an key
+
+console.log(kashi.Name);// 1st method
+console.log(kashi["Name"]);//2nd method
+
